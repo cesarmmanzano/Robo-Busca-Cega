5c8866d61b23bea7051a2becabc3dcc9dc5841be
AnyVictor <dest1933@gmail.com>
2021-04-11 13:30:27 -0300

    Adicionar leitura do arquivo

--- Codigo/teste.py
+++ Codigo/teste.py
@@ -1,1 +1,10 @@
-print('teste')
+with open('index.txt', 'r') as f:
+    ws, hs = [int(x) for x in next(f).split(',')]
+    we, he = [int(x) for x in next(f).split(',')]
+    start = [ws,hs]
+    end = [we,he]
+    l = [[int(num) for num in line.split(',')] for line in f]
+
+print(start)
+print(end)
+print(l)
